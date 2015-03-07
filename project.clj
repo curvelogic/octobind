a3775a82c4061a67002ea52eb7b2c3c555c55a13
gmorpheme <github@gmorpheme.net>
2015-03-07 11:05:05 +0000

    Update project settings.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject octobind "0.1.0-SNAPSHOT"
   :description "Multibind and similar helpers for com.stuartsierra.component library"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/curvelogic/octobind"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.1"]
                  [com.stuartsierra/component "0.2.2"]])
